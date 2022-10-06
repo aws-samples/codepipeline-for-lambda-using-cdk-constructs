2785b0a6070db2d161ce2c2d57f32a90302b0644
EC2 Default User <ec2-user@ip-172-31-32-17.ec2.internal>
2022-10-06 21:46:27 +0000

    making changes based on roles

--- application-developer/team-a-lambda-pipeline/lib/team-a-lambda-pipeline-stack.ts
+++ application-developer/team-a-lambda-pipeline/lib/team-a-lambda-pipeline-stack.ts
@@ -1,11 +1,14 @@
 import { App, Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Repository, Code } from 'aws-cdk-lib/aws-codecommit'
 import * as lambdapipeline from 'lambda-pipeline-construct';
 import * as config from '../config/config.json';
+import path = require('path');
 
 const app = new App()
 const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: config.region };
 const general = `${config.orgName}-${config.appName}-${config.attribute}`.toLowerCase();
+
 
 export class teamALambdaPipelineStack extends lambdapipeline.LambdaPipelineConstruct {
   constructor(scope: Construct, id: string, props: lambdapipeline.LambdaPipelineConstructProps = {
@@ -21,8 +24,22 @@ export class teamALambdaPipelineStack extends lambdapipeline.LambdaPipelineConst
     buildSpecPathECR: config.buildSpecPathECR,
     buildSpecPathLambda: config.buildSpecPathLambda,
     lambdaArn: ""
-  }) {
-    super(scope, id, props)
+  }){
+    super(scope, id, props);
+    
+    const repo = new Repository(this, `${general}-codecommit-repo`, {
+      repositoryName: `${general}-repo`,
+      description: `${general}-repo`,
+      code: Code.fromDirectory(path.join(__dirname, '../bootstrap/'), config.branchName)
+    })
+    new CfnOutput(this, `${general}-codercommit-repo-name`, { value: repo.repositoryName })
+
+    const ecrrepo = new Repository(this, `${general}-ECRRepo`, {
+      repositoryName: `${general}-ecr-repo`,
+      // imageScanOnPush: true,
+    });
+    new CfnOutput(this, `${general}-ecr-repo-name`, { value: ecrrepo.repositoryName} )
+    
   }
 }
 
